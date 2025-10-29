84a1ab14844b3d230222f90250ee3d7d40ba0a8d
emmettmyts <emmett@mytsapp.com>
2025-10-28 23:54:33 -0400

    Fix e2e test error

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,7 +30,7 @@ export default defineConfig({
 
   webServer: {
     command: 'npm run dev',
-    url: 'http://localhost:3001',
+    url: process.env.BASE_URL || 'http://localhost:3000',
     reuseExistingServer: !process.env.CI,
     timeout: 120000,
   },
